2c56ae0e9fa997f6c050e33fdfaae66bbc8c5c43
Tanu-priya02 <121554388+Tanu-priya02@users.noreply.github.com>
2024-12-30 12:41:15 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Dashboard } from './pages/Dashboard';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
